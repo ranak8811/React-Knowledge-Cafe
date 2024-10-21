a560fb639ff7663ad72290a6979dbd467ca204c3
Anwar_Hossain <ranakrphone@gmail.com>
2024-10-22 00:49:23 +0600

    json data created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl bg-yellow-200">Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
